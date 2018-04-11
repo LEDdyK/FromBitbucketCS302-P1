27e45f1b929ea3a3b717e361216e74278cd1cde0
litekiwiyoghurt <lkim564@aucklanduni.ac.nz>
2018-04-11 21:04:26 +1200

    Update AI controller: act upon reaching an intersection

--- src/PacmanRip/AiController.java
+++ src/PacmanRip/AiController.java
@@ -2,21 +2,69 @@ package PacmanRip;
 
 public class AiController {
 	
-	public static void controlMom(Enemy mom, Player player) {
-		//act upon mode:scatter
-		//move to a designated target
-		if (mom.getMode() == 1) {
+	public static void controlMom(Enemy mom, Player player, int[][] map) {
+		//act when within 50px of intersection
+		boolean upAvail, downAvail, leftAvail, rightAvail;
+		int[] intersectPos = new int[2];
+		int[] target = new int[2];
+		
+		if ((map[mom.getXPos() + mom.getXVel() * 50][mom.getYPos()] == 2) && (mom.getXVel() != 0)) {
+			intersectPos[0] = mom.getXPos() + mom.getXVel() * 50;
+			intersectPos[1] = mom.getYPos();
+			//find available directions at intersection
+			//up
+			if (map[mom.getXPos() + mom.getXVel() * 50][mom.getYPos() - 100] == 0) {
+				upAvail = true;
+			} else upAvail = false;
+			//down
+			if (map[mom.getXPos() + mom.getXVel() * 50][mom.getYPos() + 100] == 0) {
+				downAvail = true;
+			} else downAvail = false;
+			//left
+			if ((map[mom.getXPos() + mom.getXVel() * 50 - 100][mom.getYPos()] == 0) && (mom.getXVel() != 1)) {
+				leftAvail = true;
+			} else leftAvail = false;
+			//right
+			if ((map[mom.getXPos() + mom.getXVel() * 50 + 100][mom.getYPos()] == 0) && (mom.getXVel() != -1)) {
+				rightAvail = true;
+			} else rightAvail = false;
+			//set available directions
+			mom.setAvailDir(upAvail, downAvail, leftAvail, rightAvail);
+			boolean[] checkDir = new boolean[] {upAvail, downAvail, leftAvail, rightAvail};
+			System.out.println("current position: " + mom.getXPos() + " " + mom.getYPos());
+			System.out.println("up:" + checkDir[0] + " down:" + checkDir[1] + " left:" + checkDir[2] + " right:" + checkDir[3]);
+			
+			//act upon mode:scatter
+			//move to a designated target
+			if (mom.getMode() == 1) {
+				target[0] = 500;
+				target[1] = 700;
+				mom.setNextDir(mom.getAvailDir(), intersectPos, target, 100);
+			}
+			//act upon mode:chase
+			//move towards the player
+			else if (mom.getMode() == 2) {
+				
+			}
+			//act upon mode:run
+			//run away from player
+			else {
+				
+			}
+			mom.setXPos(mom.getXPos() + (mom.getXVel() * mom.getVelMag()));
+			mom.setYPos(mom.getYPos() + (mom.getYVel() * mom.getVelMag()));
+		}
+		else if ((map[mom.getXPos()][mom.getYPos() + mom.getYVel() * 50] == 2) && (mom.getYVel() != 0)) {
 			
 		}
-		//act upon mode:chase
-		//move towards the player
-		else if (mom.getMode() == 2) {
-			
+		else if (map[mom.getXPos()][mom.getYPos()] == 2) {
+			mom.changeDir(mom.getNextDir());
+			mom.setXPos(mom.getXPos() + (mom.getXVel() * mom.getVelMag()));
+			mom.setYPos(mom.getYPos() + (mom.getYVel() * mom.getVelMag()));
 		}
-		//act upon mode:run
-		//run away from player
 		else {
-			
+			mom.setXPos(mom.getXPos() + (mom.getXVel() * mom.getVelMag()));
+			mom.setYPos(mom.getYPos() + (mom.getYVel() * mom.getVelMag()));
 		}
 //		int xDif = player.getXPos() - mom.getXPos();
 //		int yDif = player.getYPos() - mom.getYPos();
--- src/PacmanRip/Main.java
+++ src/PacmanRip/Main.java
@@ -9,7 +9,8 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -30,15 +31,53 @@ public class Main extends Application {
 		//Canvas size: Minimum = 1024x738, Maximum = 1440x900
 		Canvas canvas = new Canvas(1024, 768);
 		root.getChildren().add(canvas);
-
+		
+		int[][] map = new int[1024][768];
 		//Scene graphics
 		GraphicsContext graphics = canvas.getGraphicsContext2D();
 		Image circle = new Image("circle.png");
 		Image circleE = new Image("circle.png");
+		Rectangle turnTile = new Rectangle(500, 100, 100, 100);
+		turnTile.setFill(Color.GREEN);
+		root.getChildren().add(turnTile);
+		map[500][100] = 2;
+		
+		//walls for visual debugging purposes
+		Rectangle[] walls = new Rectangle[8];
+		walls[1] = new Rectangle(400, 0, 100, 100);//top left
+		walls[1].setFill(Color.RED);
+		map[400][0] = 1;
+		walls[3] = new Rectangle(600, 0, 100, 100);//top right
+		walls[3].setFill(Color.RED);
+		map[600][0] = 1;
+		walls[5] = new Rectangle(600, 200, 100, 100);//bottom right
+		walls[5].setFill(Color.RED);
+		map[600][200] = 1;
+		walls[7] = new Rectangle(400, 200, 100, 100);//bottom left
+		walls[7].setFill(Color.RED);
+		map[400][200] = 1;
+		root.getChildren().addAll(walls[1], walls[3], walls[5], walls[7]);
+		//toggle walls
+		walls[2] = new Rectangle(500, 0, 100, 100);//top
+		walls[2].setFill(Color.RED);
+		map[500][0] = 1;
+		root.getChildren().add(walls[2]);
+//		walls[4] = new Rectangle(600, 100, 100, 100);//right
+//		walls[4].setFill(Color.RED);
+//		map[600][100] = 1;
+//		root.getChildren().add(walls[4]);
+//		walls[6] = new Rectangle(500, 200, 100, 100);//bottom
+//		walls[6].setFill(Color.RED);
+//		map[500][200] = 1;
+//		root.getChildren().add(walls[6]);
+//		walls[8] = new Rectangle(400, 100, 100, 100);//left
+//		walls[8].setFill(Color.RED);
+//		map[400][100] = 1;
+//		root.getChildren().add(walls[8]);
 		
 		Player pacman = new Player(1, 512, 384, 1, 0, 4);
-		Enemy blinky = new Enemy(7, 0, 0, 1, 0, 1);
-		blinky.setMode(2);
+		Enemy blinky = new Enemy(7, 0, 100, 1, 0, 1);
+		blinky.setMode(1);
 		
 		//Event handler
 		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
@@ -71,13 +110,13 @@ public class Main extends Application {
 		});
 		
 		//Get system time
-		final long startNanoTime = System.nanoTime();
+		//final long startNanoTime = System.nanoTime();
 		//long newNanoTime = currentNanoTime;
 		
-		//window dynamics
+		//window dynamics: fps = 60
 		new AnimationTimer() {
 			public void handle(long currentNanoTime) {
-				
+				//fps limiter
 				if (pacman.getTick() < 1) {
 					pacman.setTick(pacman.getTick() + 1);
 				}
@@ -86,7 +125,11 @@ public class Main extends Application {
 					//update actions
 					pacman.setXPos(pacman.getXPos() + (pacman.getXVel() * pacman.getVelMag()));
 					pacman.setYPos(pacman.getYPos() + (pacman.getYVel() * pacman.getVelMag()));
-					AiController.controlMom(blinky, pacman);
+					AiController.controlMom(blinky, pacman, map);
+					System.out.println(blinky.getXPos() + " " + blinky.getYPos());
+//					if (map[blinky.getXPos()][blinky.getYPos()] == 2) {
+//						System.out.println("it works though...");
+//					}
 					
 					graphics.clearRect(0, 0, 1024, 768);
 					graphics.drawImage(circle, pacman.getXPos(), pacman.getYPos());
