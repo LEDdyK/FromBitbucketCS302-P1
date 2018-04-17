ecbfec1b2b6dc477eb9395b5f484664db4c547ed
litekiwiyoghurt <lkim564@aucklanduni.ac.nz>
2018-04-18 09:46:06 +1200

    newBranch

--- src/PacmanRip/Main.java
+++ src/PacmanRip/Main.java
@@ -11,8 +11,6 @@ import javafx.scene.image.Image;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
